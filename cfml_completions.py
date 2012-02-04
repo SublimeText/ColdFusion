be3bb43d951fa82715dead6a472b6fdf9bbc74f9
atomi <atomi@lavabit.com>
2012-02-03 23:30:41 -0800

    changed class name just in case it conflicts with cfml completions in any way

--- cfml_completions.py
+++ cfml_completions.py
@@ -1,7 +1,7 @@
 import sublime, sublime_plugin
 
 # Provide completions that match just after typing an opening angle bracket
-class TagCompletions(sublime_plugin.EventListener):
+class CfmlTagCompletions(sublime_plugin.EventListener):
     def on_query_completions(self, view, prefix, locations):
         # Only trigger within CFML
         if not view.match_selector(locations[0],
