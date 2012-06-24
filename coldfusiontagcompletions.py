0a53bb8770f797d51d7109c015f2287ff4f36b3b
atomi <atomi@lavabit.com>
2012-06-24 16:09:12 -0700

    strip whitespace please

--- coldfusiontagcompletions.py
+++ coldfusiontagcompletions.py
@@ -2,21 +2,21 @@ import sublime
 import sublime_plugin
 class ColdFusionTagComplete(sublime_plugin.EventListener):
     valid_scopes_tags = ["meta.tag.inline.cfml", "meta.tag.block.other.cfml", "meta.tag.block.function.cfml", "meta.tag.block.flow-control.cfml", "meta.tag.block.exceptions.cfml"]
- 
-  
+
+
     def on_modified(self, view):
         sel = view.sel()[0].a
         if view.substr(sel - 1) == " ":
             if any(s in view.scope_name(sel) for s in self.valid_scopes_tags):
-                sublime.set_timeout(lambda: 
+                sublime.set_timeout(lambda:
                     view.run_command("auto_complete", { 'disable_auto_insert': True, 'next_completion_if_showing': False }), 50
                 )
-               
+
 
     def on_query_completions(self, view, prefix, locations):
         sel = view.sel()[0].a
         completions = []
-        
+
         if any(s in view.scope_name(sel) for s in self.valid_scopes_tags):
             scopestart = view.extract_scope(sel).a
             if scopestart == sel:
@@ -33,8 +33,8 @@ class ColdFusionTagComplete(sublime_plugin.EventListener):
         module = ".".join(parts[:-1])
         m = __import__( module )
         for comp in parts[1:]:
-            m = getattr(m, comp)            
+            m = getattr(m, comp)
         return m
 
     cflib = get_class('taglib.cf10.tags')()
-    
+
