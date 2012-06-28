349ce931339ce2c2c50676cb7bb84f016fe6cc50
atomi <atomi@lavabit.com>
2012-06-28 16:10:04 -0700

    adds cfscript component completions

---
+++ functioncompletions.py
@@ -0,0 +1,42 @@
+import sublime
+import sublime_plugin
+import re
+
+completions = []
+
+def add_methods(cfc_file):
+    with open(cfc_file, 'r') as f:
+        read_data = f.read()
+    methods = []
+    method_lines = re.findall('function\s[^{]+', read_data)
+
+    for l in method_lines:
+        l = re.sub("[\\n|\s]+"," ",l)
+        s = re.search('(\w+)\s?\(.*\)', l)
+        if s:
+            methods.append(s.group().strip())
+
+    completions.extend(methods)
+
+class MethodsAutoComplete(sublime_plugin.EventListener):
+    def on_query_completions(self, view, prefix, locations):
+        if not view.match_selector(locations[0],
+                "source.cfscript.cfc - meta - string - comment"):
+            return []
+
+        _completions = []
+        add_methods(view.file_name())
+
+        for c in completions:
+            snippet = c
+            params = re.sub("\w+\(","",snippet,1)[:-1].split(",")
+
+            num = 1
+            for p in params:
+                snippet = snippet.replace(p, '${' + str(num) + ':' + p + '}')
+                num = num + 1
+            c = re.sub("\(.*\)","",c)
+            _completions.append((c + "\tFunction (component)", snippet))
+
+        del completions[:]
+        return _completions
